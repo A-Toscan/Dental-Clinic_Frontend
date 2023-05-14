17855e32f53132a2f232a041e07d7feaf7d72c3d
Toscan <dipointg@gmail.com>
2023-05-14 20:13:16 +0200

    fixing the problem with the Appointments through the use of some new hooks

--- src/containers/appointments/Appointments.jsx
+++ src/containers/appointments/Appointments.jsx
@@ -20,8 +20,7 @@ export default function Appointments() {
     } else {
       navigate("/");
     }
-
-}, [allAppointments]);
+  }, [allAppointments]);
 
   const getAllAppointments = async (token, user_id) => {
     try {
